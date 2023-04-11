9ded1eb924ef9d9dee2be38a56d10b4419041978
Vitaly <vitalylativ@yandex.ru>
2023-04-11 11:30:33 +0000

    Added exemplary deploy and trackEvent scripts

---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+const { deployProofMarketFixture } = require('../test/fixtures');
+const fs = require('fs');
+
+async function main() {
+    const { proofMarket, token, owner, user, producer, relayer } = await deployProofMarketFixture();
+
+    console.log('MockTocken deployed to:', token.address);
+    console.log('ProofMarketEndpoint deployed to:', proofMarket.address);
+    console.log('Owner:', owner.address);
+    console.log('User:', user.address);
+    console.log('Producer:', producer.address);
+    console.log('Relayer:', relayer.address);
+
+    const addresses = {
+        token: token.address,
+        proofMarket: proofMarket.address,
+    };
+    fs.writeFileSync('deployed_addresses.json', JSON.stringify(addresses, null, 2));
+}
+    
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
