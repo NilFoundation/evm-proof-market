3d6bb06d56a97daa24bf3161e5cb5139b2ba84ce
Vitaly <vitalylativ@yandex.ru>
2023-05-22 16:03:53 +0000

    Added relayer script and its description

---
+++ scripts/relayer/relayer.js
@@ -0,0 +1,27 @@
+
+const path = require('path');
+const { spawn } = require('child_process');
+
+const scripts = [
+    path.join(__dirname, 'priceRelay.js'),
+    path.join(__dirname, 'orderRelay.js'),
+    path.join(__dirname, 'proofRelay.js')
+];
+
+function runScript(script) {
+    const child = spawn('node', [script]);
+
+    child.stdout.on('data', (data) => {
+        console.log(`Output from ${script}: ${data}`);
+    });
+
+    child.stderr.on('data', (data) => {
+        console.error(`Error from ${script}: ${data}`);
+    });
+
+    child.on('close', (code) => {
+        console.log(`Script ${script} exited with code ${code}`);
+    });
+}
+
+scripts.forEach(runScript);
