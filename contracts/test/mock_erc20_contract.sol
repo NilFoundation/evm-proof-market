c2a87f60e977a345d3b4540e4b848eb8998c5661
Haresh G <haresh.gedia@gmail.com>
2023-07-10 10:38:30 +0530

    Clean up typos/refactor

--- contracts/test/mock_erc20_contract.sol
+++ contracts/test/mock_erc20_contract.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MockTocken is ERC20 {
-    constructor() ERC20("MockTocken", "MTK") {}
+contract MockToken is ERC20 {
+    constructor() ERC20("MockToken", "MTK") {}
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
